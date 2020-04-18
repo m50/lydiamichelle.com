583e02091006b9977436d911a93b7600bd99c323
Marisa Clardy <marisa@clardy.eu>
2020-04-17 22:05:23 -0500

    🔧 Mess with the postcss config.

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,21 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+    content: [
+        // Jekyll output directory
+        './_site/**/*.html',
+    ],
+    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+});
+
 module.exports = {
     plugins: [
         require("postcss-import"),
         require('postcss-nested'),
         require("tailwindcss")("tailwind.config.js"),
-        require("autoprefixer")
+        require('cssnano')(),
+        require("autoprefixer"),
+        ...process.env.NODE_ENV === 'production'
+            ? [purgecss]
+            : []
     ]
 };
+

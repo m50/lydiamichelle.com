f7c512c0c01b0973950a39e6a1a15d5d452dc6f6
Marisa Clardy <marisa@clardy.eu>
2021-03-02 00:10:36 -0600

    Finish series page.

--- components/templates/Series/ImageGrid.tsx
+++ components/templates/Series/ImageGrid.tsx
@@ -1,12 +1,44 @@
 import React from 'react';
 import { ImageInfo } from '../../../types/Series';
-import Image from 'next/image'
+import Image from 'next/image';
+import slugify from 'slugify';
 
-export const ImageGrid: React.FC<{ images: ImageInfo[] }> = ({ images }) => {
+interface Props {
+  images: ImageInfo[];
+  onImageClick: (image: ImageInfo) => void
+}
+
+const captionClasses = `
+  top-0 left-0 h-16 xl:h-full w-full xl:absolute transform transition-opacity rounded-xl
+  duration-300 ease-in-out text-center xl:opacity-0 hover:opacity-100 text-white
+  flex flex-col justify-center -mt-16 xl:mt-0
+`;
+
+export const ImageGrid: React.FC<Props> = ({ images, onImageClick }) => {
   return (
     <section className="w-full bg-white" data-turbolinks="false">
       <div className="w-full md:w-2/3 mx-auto grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4 p-5">
-        {/* Loop throw images and add them here. */}
+        {images.map((image) => {
+          const slug = slugify(image.title, { lower: true, strict: true, locale: 'en' });
+          return (
+            <div key={slug}>
+              <a onClick={() => onImageClick(image)} href={`#${slug}`}>
+                <figure className="relative w-full h-88 md:h-64 2xl:h-88 rounded-xl overflow-hidden">
+                  <Image layout="fill" className="h-full w-full object-cover object-center rounded-xl"
+                    src={image.image} alt={image.title + ' ' + image.blurb} />
+                  <figcaption className={captionClasses} style={{ backgroundColor: "rgba(0,0,0,0.75);" }}>
+                    <cite className="text-lg xl:text-3xl font-serif capitalize">
+                      <span className="sr-only">Piece: </span>{image.title}
+                    </cite>
+                    <small className="text-sm xl:text-lg capitalize">
+                      <span className="sr-only">Medium: </span>{image.medium}
+                    </small>
+                  </figcaption>
+                </figure>
+              </a>
+            </div>
+          )
+        })}
       </div>
     </section>
   );
--- components/templates/Series/index.tsx
+++ components/templates/Series/index.tsx
@@ -1,20 +1,46 @@
-import * as React from 'react';
-import { Series } from '../../../types/Series';
+import React, { useState, useEffect } from 'react';
+import { ImageInfo, Series } from '../../../types/Series';
 import Head from 'next/head';
 import Header from './Header';
 import Body from './Body';
 import ImageGrid from './ImageGrid';
+import ImageModal from './ImageModal';
+import slugify from 'slugify';
 
-export const SeriesTemplate: React.FC<Series> = (series) => (
-  <article>
-    <Head>
-      <title>{series.title} | Lydia Michelle Art</title>
-      <meta property="og:image" content={series.images[0]?.image} />
-    </Head>
-    <Header {...series} />
-    <Body content={series.body} />
-    <ImageGrid images={series.images} />
-  </article>
-);
+const slug = (i: string) => slugify(i, { lower: true, strict: true, locale: 'en' });
+
+export const SeriesTemplate: React.FC<Series> = (series) => {
+  const [selectedImage, setSelectedImage] = useState<ImageInfo | null>(null);
+
+  useEffect(() => {
+    if (window.location.hash) {
+      const image = series.images.find((i) => '#' + slug(i.title) === window.location.hash);
+      if (image) {
+        setSelectedImage(image);
+      }
+    }
+  }, [series.images]);
+
+  const onImageClick = (image: ImageInfo) => {
+    setSelectedImage(image);
+  };
+  const onOutsideClick = () => {
+    setSelectedImage(null);
+    window.history.pushState("", document.title,
+      window.location.pathname + window.location.search);
+  }
+  return (
+    <article>
+      <Head>
+        <title>{series.title} | Lydia Michelle Art</title>
+        <meta property="og:image" content={series.images[0]?.image} />
+      </Head>
+      <Header {...series} />
+      <Body content={series.body} />
+      <ImageGrid images={series.images} onImageClick={onImageClick} />
+      {selectedImage && <ImageModal onClickAway={onOutsideClick} image={selectedImage} />}
+    </article>
+  );
+};
 
 export default SeriesTemplate;

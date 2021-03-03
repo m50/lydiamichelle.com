4624b33047ee5403eaafce79a9e591795312b727
Marisa Clardy <marisa@clardy.eu>
2021-03-03 00:08:48 -0600

    Disable optimize API for now. This will need to be played with a bit more in the future.

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -3,5 +3,6 @@ import React, { DetailedHTMLProps, ImgHTMLAttributes } from 'react';
 type ImgProps = DetailedHTMLProps<ImgHTMLAttributes<HTMLImageElement>, HTMLImageElement>;
 
 export const Image: React.FC<ImgProps> = ({ src, ...props }) => (
-  <img src={`/api/optimize?image=${encodeURIComponent(src as string)}`} {...props} />
+  <img src={src} {...props} />
 );
+// src={`/api/optimize?image=${encodeURIComponent(src as string)}`}

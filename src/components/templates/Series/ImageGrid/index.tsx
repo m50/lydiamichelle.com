4abb475a2ac3099f7531e10a225e35b7aa35fd09
Marisa Clardy <marisa@clardy.eu>
2021-03-05 13:18:51 -0600

    Fix the type import of imagegrid.

--- src/components/templates/Series/ImageGrid/index.tsx
+++ src/components/templates/Series/ImageGrid/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ImageInfo } from '../../../types/Series';
+import type { ImageInfo } from '../../../../types/Series';
 import slugify from 'slugify';
 import { Image } from 'components/Image';
 import { DisplayImage } from './DisplayImage';

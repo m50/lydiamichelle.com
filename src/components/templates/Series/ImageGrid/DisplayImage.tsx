0b9bde6fc78f10d5898986f6e113d64cdd9e7f70
Marisa Clardy <marisa@clardy.eu>
2021-03-05 13:16:41 -0600

    Break out the image from the grid, and adjust styling.

---
+++ src/components/templates/Series/ImageGrid/DisplayImage.tsx
@@ -0,0 +1,39 @@
+import { Image } from 'components/Image';
+import React from 'react';
+import slugify from 'slugify';
+import { ImageInfo } from 'types/Series';
+
+interface Props {
+  image: ImageInfo;
+  onImageClick: (image: ImageInfo) => void;
+}
+
+const captionClasses = `
+  top-0 left-0 h-16 xl:h-full w-full xl:absolute transform transition-opacity rounded-xl
+  duration-300 ease-in-out text-center xl:opacity-0 hover:opacity-100 text-white
+  flex flex-col justify-center -mt-16 xl:mt-0 rounded-t-none
+`;
+
+export const DisplayImage: React.FC<Props> = ({ image, onImageClick }) => {
+  {
+    const slug = slugify(image.title, { lower: true, strict: true, locale: 'en' });
+    return (
+      <div>
+        <a onClick={() => onImageClick(image)} href={`#${slug}`}>
+          <figure className="relative w-full h-88 md:h-64 2xl:h-88 rounded-xl overflow-hidden">
+            <Image className="h-full w-full object-cover object-center rounded-xl"
+              src={image.image} alt={image.title} loading="lazy" height={400} />
+            <figcaption className={captionClasses} style={{ backgroundColor: "rgba(0,0,0,0.75)" }}>
+              <cite className="text-lg xl:text-3xl font-serif capitalize">
+                <span className="sr-only">Piece: </span>{image.title}
+              </cite>
+              <small className="text-sm xl:text-lg capitalize">
+                <span className="sr-only">Medium: </span>{image.medium}
+              </small>
+            </figcaption>
+          </figure>
+        </a>
+      </div>
+    )
+  };
+}
---
+++ src/components/templates/Series/ImageGrid/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { ImageInfo } from '../../../types/Series';
+import slugify from 'slugify';
+import { Image } from 'components/Image';
+import { DisplayImage } from './DisplayImage';
+
+interface Props {
+  images: ImageInfo[];
+  onImageClick: (image: ImageInfo) => void
+}
+
+export const ImageGrid: React.FC<Props> = ({ images, onImageClick }) => {
+  return (
+    <section className="w-full bg-white" data-turbolinks="false">
+      <div className="w-full md:w-2/3 mx-auto grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4 p-5">
+        {images.map((image, idx) => <DisplayImage image={image} onImageClick={onImageClick} key={idx} />)}
+      </div>
+    </section>
+  );
+};
+
+export default ImageGrid;

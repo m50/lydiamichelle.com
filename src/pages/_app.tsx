6731dccf2129eaf0d0ba37c5f6dfe8d83396bd51
Marisa Clardy <marisa@clardy.eu>
2021-03-11 00:45:45 -0600

    Fix lang, and add description meta tag.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -39,6 +39,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <DefaultTemplate>
       <Head>
+        <html lang="en" />
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -48,6 +49,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <link rel="icon" type="image/png" sizes="16x16" href="/imgs/favicon/favicon-16x16.png" />
         <link rel="manifest" href="/imgs/favicon/site.webmanifest" />
         <meta property="og:title" content="Lydia Michelle Art" />
+        <meta name="description" content="Portfolio website for Lydia Michelle" />
         <meta property="og:description" content="Portfolio website for Lydia Michelle" />
         <meta property="og:url" content="https://lydiamichelle.art/" />
         <meta property="og:type" content="website" />

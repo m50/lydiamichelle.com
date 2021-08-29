12fc5d8a894c0ddb95aa37c3b6f02733906d20a5
Marisa Clardy <marisa@clardy.eu>
2021-08-28 19:51:53 -0500

    Add an image proxy to the instagram page.

---
+++ src/pages/api/insta/proxy.ts
@@ -0,0 +1,13 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const url = decodeURIComponent((req.query as any).img as string);
+  const img = await fetch(url);
+
+  res.status(img.status)
+    .setHeader('content-type', img.headers.get('content-type') as string)
+    .setHeader('cache-control', img.headers.get('cache-control') as string)
+    .setHeader('last-modified', img.headers.get('last-modified') as string)
+    .setHeader('cross-origin-resource-policy', img.headers.get('cross-origin-resource-policy') as string)
+    .end(new Uint8Array(await img.arrayBuffer()));
+};

26aec85e78928621d88126baf29865e3f26f68e1
Marisa Clardy <marisa@clardy.eu>
2021-08-28 21:29:08 -0500

    Remove console.log

--- src/pages/api/insta/proxy.ts
+++ src/pages/api/insta/proxy.ts
@@ -4,8 +4,6 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   const url = decodeURIComponent((req.query as any).img as string);
   const img = await fetch(url);
 
-  console.log(res.setHeader);
-
   res.setHeader('content-type', img.headers.get('content-type') as string);
   res.setHeader('cache-control', img.headers.get('cache-control') as string);
   res.setHeader('last-modified', img.headers.get('last-modified') as string);

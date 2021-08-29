9479674d41260203e223f4af7455f98b863075cf
Marisa Clardy <marisa@clardy.eu>
2021-08-28 21:21:11 -0500

    Move setHeaders before status?

--- src/pages/api/insta/proxy.ts
+++ src/pages/api/insta/proxy.ts
@@ -4,10 +4,14 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   const url = decodeURIComponent((req.query as any).img as string);
   const img = await fetch(url);
 
-  res.status(img.status)
+  console.log(res);
+
+  res
     .setHeader('content-type', img.headers.get('content-type') as string)
     .setHeader('cache-control', img.headers.get('cache-control') as string)
     .setHeader('last-modified', img.headers.get('last-modified') as string)
-    .setHeader('cross-origin-resource-policy', img.headers.get('cross-origin-resource-policy') as string)
+    .setHeader('cross-origin-resource-policy', img.headers.get('cross-origin-resource-policy') as string);
+
+  res.status(img.status)
     .end(new Uint8Array(await img.arrayBuffer()));
 };
